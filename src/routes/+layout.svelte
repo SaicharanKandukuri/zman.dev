78919221a53dfc3d0e49af9fff98e2e28c82487a
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-12-16 09:53:17 +0530

    chore: commit things before migration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
   import "../app.css";
+  import { MetaTags } from 'svelte-meta-tags';
 </script>
 
+<MetaTags title="zman" description="zman aka saicharankandukuri" />
 <div class="dark">
   <slot ><!-- optional fallback --></slot>
 </div>
