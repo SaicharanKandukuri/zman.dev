8eccedbb99a0186eeedbbe1de36283f0812d6d15
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2025-02-09 12:29:07 +0530

    chore: add darkmode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 import RetroGrid from "@/components/magicui/retro-grid";
-import Image from "next/image";
+import BearButton from "@/components/bearcheckbox/bear";
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center p-24">
-      <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-5xl md:text-9xl font-bold leading-none tracking-tighter text-transparent">
-        zman.dev
-      </span>
+    <main className=" flex min-h-screen flex-col items-center justify-center p-24">
+        <div className="flex">
+            <span className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-5xl md:text-9xl font-bold leading-none tracking-tighter text-transparent">
+                zman.dev
+            </span>
+            <div className="-bottom-0">
+                <BearButton />
+            </div>
+        </div>
       <RetroGrid />
     </main>
   );
